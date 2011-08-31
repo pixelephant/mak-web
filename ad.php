7e7fafe0ad33b29c089e38499169970b39e69045
Norbert Pap <norbert.pap@pixelephant.hu>
2011-08-31 11:54:46 +0200

    anim gifek
    Signed-off-by: Norbert Pap <norbert.pap@pixelephant.hu>

--- ad.php
+++ ad.php
@@ -1,4 +1,4 @@
-<a target="_blank" href="http://www.arceurope.com/EN/memberservices.aspx"><img class="ad" src="img/ad/arc.jpg" alt="ARC europe - Show your card!" /></a>
-<a target="_blank" href="http://www.erscharter.eu/"><img class="ad" src="img/ad/ersc.jpg" alt="European road safety charter" /></a>
-<a target="_blank" href="https://www.generali.hu/GeneraliBiztosito.aspx"><img class="ad" src="img/ad/generali.jpg" alt="Generali biztosító" /></a>
+<a target="_blank" href="http://www.arceurope.com/EN/memberservices.aspx"><img class="ad" src="img/ad/arc.gif" alt="ARC europe - Show your card!" /></a>
+<a target="_blank" href="http://www.erscharter.eu/"><img class="ad" src="img/ad/ersc.gif" alt="European road safety charter" /></a>
+<a target="_blank" href="https://www.generali.hu/GeneraliBiztosito.aspx"><img class="ad" src="img/ad/generali.gif" alt="Generali biztosító" /></a>
 <iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FFrontline-m%25C3%25A9dia-Kft%2F134495689944678&amp;width=200&amp;colorscheme=light&amp;show_faces=true&amp;border_color=black&amp;stream=true&amp;header=true&amp;height=427" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:200px; height:427px; background:white; margin: 0 auto; display: block;" allowTransparency="true"></iframe>
