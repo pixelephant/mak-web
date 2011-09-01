ea59a3f9b909f566e004ff09f21c287a179aef61
Balázs Antal <balazs.antal@pixelephant.hu>
2011-09-01 09:38:16 +0200

    admin friss

--- admin/usermanagement/index.php
+++ admin/usermanagement/index.php
@@ -1,31 +1,21 @@
-<!DOCTYPE HTML>
-<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!-->
-<html class="no-js" lang="en">
-	<!--<![endif]-->
-	<head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="shortcut icon" href="/favicon.ico">
-		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-		<meta charset="UTF-8">
-		<meta content="Kulcsszó1, Kulcsszó2, Kulcsszó3" name="keywords"><meta content="Description szövege jön ide..." name="description">
-		
-		<title>User Management</title>		
-		<link rel="stylesheet" href="lib/css/reset.css" />
-		<link rel="stylesheet" href="lib/css/main.css" />
-		<link rel="stylesheet" href="css/ui.jqgrid.css" />
-		<script src="lib/js/modernizr-2.min.js"></script>
-	</head>
-	<body>
-	<div id="content">
-		<table id="usermanagement"></table>
-	</div>
-	
-	<script type="text/javascript" src="js/jquery-1.6.2.js"></script>
-	<script type="text/javascript" src="js/jquery-ui-1.8.9.js"></script>
-	<script type="text/javascript" src="js/grid.locale-hu.js"></script>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+  <head>
+    <title>User Management</title>
+    
+    <link rel="stylesheet" href="smoothness/smoothness.css" />
+    <link rel="stylesheet" href="css/ui.jqgrid.css" />
+    <script type="text/javascript" src="js/jquery-1.6.2.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.8.9.js"></script>
+    <script type="text/javascript" src="js/grid.locale-hu.js"></script>
 	<script type="text/javascript" src="js/jquery.jqGrid.min.js"></script>
-	<script type="text/javascript" src="js/usermanagement.js"></script>
-	</body>
+    <script type="text/javascript" src="js/usermanagement.js"></script>
+  </head>
+  <body>
+    <div id="content">
+		<table id="usermanagement"></table>
+		<div id="pager"></div>
+    </div>
+  </body>
+</html>
