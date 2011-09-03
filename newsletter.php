c31c3ac66bde7d4c40814dca2ddf50cf0a8a0882
Balázs Antal <balazs.antal@pixelephant.hu>
2011-09-03 15:14:12 +0200

    Twitter + Face blank-ba nyílik

--- newsletter.php
+++ newsletter.php
@@ -1,6 +1,6 @@
 <div class="newsletter">
-	<a href="http://facebook.com" id="facebook"><img height="40" src="img/fb.png" alt="Facebook" /></a>
-	<a href="http://twitter.com" id="twitter"><img height="40" src="img/tw.png" alt="Twitter" /></a>
+	<a href="http://facebook.com" id="facebook" target="_blank"><img height="40" src="img/fb.png" alt="Facebook" /></a>
+	<a href="http://twitter.com" id="twitter" target="_blank"><img height="40" src="img/tw.png" alt="Twitter" /></a>
 	<a id="newsletterbutton" href="#"><span>Hírlevél</span> <em></em></a>
 	<form action="#">
 		<div>
