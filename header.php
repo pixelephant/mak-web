3dafc18f5190595f48d413f499f37a5f6c61b3a4
Balázs Antal <balazs.antal@pixelephant.hu>
2011-09-11 13:08:44 +0200

    header ikonok, oktatas,rendezvény, klubtagság ikoncsere, hírlevél validálás

--- header.php
+++ header.php
@@ -4,10 +4,10 @@
 </div>
 <div id="headerButtons">
 	<div id="loginContainer">
-		<a href="#" id="segelyButton" class="gray-button"><img src="img/pictogram-01.png" alt="Segélyszolgálat" /></a>
-		<a href="#" id="szervizButton" class="gray-button"><img src="img/pictogram-03.png" alt="Szervizpontok" /></a>
-		<a href="#" id="kozlekButton" class="gray-button"><img src="img/pictogram-04.png" alt="Közlekedésbiztonság" /></a>
-		<a href="#" id="travelButton" class="gray-button"><img src="img/pictogram-02.png" alt="Travel" /></a>
+		<a href="segelyszolgalat" id="segelyButton" class="gray-button"><img src="img/pictogram-01.png" alt="Segélyszolgálat" /></a>
+		<a href="szervizpontok" id="szervizButton" class="gray-button"><img src="img/pictogram-03.png" alt="Szervizpontok" /></a>
+		<a href="kozlekedesbiztonsag" id="kozlekButton" class="gray-button"><img src="img/pictogram-04.png" alt="Közlekedésbiztonság" /></a>
+		<a href="travel" id="travelButton" class="gray-button"><img src="img/pictogram-02.png" alt="Travel" /></a>
 		<a href="#" data-reveal-id="loginModal" id="loginButton" class="gray-button"><span>Bejelentkezés</span><em></em></a>
 		<div id="loginModal" class="reveal-modal">
 			<form id="loginform" action="#">
--- newsletter.php
+++ newsletter.php
@@ -8,7 +8,7 @@
 	<a href="http://facebook.com" id="facebook" target="_blank"><img height="40" src="img/fb.png" alt="Facebook" /></a>
 	<a href="http://twitter.com" id="twitter" target="_blank"><img height="40" src="img/tw.png" alt="Twitter" /></a>
 	<a id="newsletterbutton" class="gray-button" href="#"><span>Hírlevél</span> <em></em></a>
-	<form action="#">
+	<form id="newsletter-form" action="#">
 		<div>
 		<input type="text" class="required email" name="newsletter-mail" id="newsletter-mail" placeholder="Email cím" /><input type="submit" value="Ok" class="yellow-button" />
 		</div>
