62df71594fb43e1ec7decb9af78d62d23732d34e
Balázs Antal <balazs.antal@pixelephant.hu>
2011-09-15 22:03:54 +0200

    checkmail

--- lib/php/checkemail.php
+++ lib/php/checkemail.php
@@ -7,14 +7,14 @@ include 'class.mak.php';
 
 $main = new mak(false);
 
-$cond['e_mail'] = trim($_POST['email']);
+$cond['e_mail'] = trim($_GET['email']);
 
 $regelt = $main->get_felhasznalo($cond,'regisztracio_ideje');
 		
-if($regelt[0]['regisztracio_ideje'] != '0000-00-00 00:00:00'){
-	echo 'false';
-}else{
+if($regelt[0]['regisztracio_ideje'] == '0000-00-00 00:00:00' || $regelt['count'] == 0 || $regelt === FALSE){
 	echo 'true';
+}else{
+	echo 'false';
 }
 
 $main->close();
