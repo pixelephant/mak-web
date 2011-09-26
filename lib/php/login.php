36d37c0033c7d08a2db6617bf5525ab35730f3e2
Balázs Antal <balazs.antal@pixelephant.hu>
2011-09-26 22:10:17 +0200

    logout proba 826

--- lib/php/login.php
+++ lib/php/login.php
@@ -13,6 +13,15 @@ if(isset($_POST['logout']) && $_POST['logout'] == 'logout'){
 	session_destroy();
 	$_SESSION = array();
 	
+	if (ini_get("session.use_cookies")) {
+	    $params = session_get_cookie_params();
+	    setcookie(session_name(), '', time() - 42000,
+	        $params["path"], $params["domain"],
+	        $params["secure"], $params["httponly"]
+	    );
+	}
+	
+	
 	if(isset($_COOKIE['user_id']) && isset($_COOKIE['keresztnev']) && isset($_COOKIE['tagsag'])){
 		$minuszEgyHonap = -60 * 60 * 24 * 30 + time();
 		setcookie('user_id','',$minuszEgyHonap);
