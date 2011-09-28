90285486d3a3a3aa2501b42bf5359c4d75670815
Balázs Antal <balazs.antal@pixelephant.hu>
2011-09-28 09:18:59 +0200

    kijelentkezés

--- lib/php/class.mak.php
+++ lib/php/class.mak.php
@@ -12,12 +12,12 @@ class mak extends db{
 
 	public function __construct($debug=false){
 		parent::__construct('','','','','',$debug);
-		/*
-		if(isset($_COOKIE['user_id']) && isset($_COOKIE['tagsag']) && isset($_COOKIE['keresztnev'])){
+		
+		if((isset($_COOKIE['user_id']) && isset($_COOKIE['tagsag']) && isset($_COOKIE['keresztnev'])) && ($_COOKIE['user_id'] != '' && $_COOKIE['tagsag'] != '' && $_COOKIE['keresztnev'] != '')){
 			$_SESSION['user_id'] = $_COOKIE['user_id'];
 			$_SESSION['tagsag'] = $_COOKIE['tagsag'];
 			$_SESSION['keresztnev'] = $_COOKIE['keresztnev'];
-		}*/
+		}
 	}
 
 	//SELECT
