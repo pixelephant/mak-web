f6b0f9de0fce69cb07e2d4bda000e8035305dbaf
Norbert Pap <norbert.pap@pixelephant.hu>
2011-09-29 15:57:51 +0200

    -.-
    Signed-off-by: Norbert Pap <norbert.pap@pixelephant.hu>

--- sikeresfizetes.php
+++ sikeresfizetes.php
@@ -210,6 +210,9 @@ window.jQuery || document.write('<script src="lib/js/jquery-1.6.2.js">\x3C/scrip
 		</script>
 		<script type="text/javascript" src="lib/js/sub.js">
 		</script>
+		<script type="text/javascript">
+			
+		</script>
 		<script>
 var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
