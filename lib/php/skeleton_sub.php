d99b3fb7fd1aa00fd3081c0ded769ec03686c0f6
Balázs Antal <balazs.antal@pixelephant.hu>
2011-10-06 13:18:38 +0200

    favico n

--- lib/php/skeleton_sub.php
+++ lib/php/skeleton_sub.php
@@ -12,6 +12,7 @@ session_start();
 	<head>
 		<base href="http://sfvm104.serverfarm.hu/mak/" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 		<meta charset="UTF-8">
 		<meta content="<?php startblock('keywords') ?><?php endblock() ?>" name="keywords">
 		<meta content="<?php startblock('description') ?><?php endblock() ?>" name="description">
